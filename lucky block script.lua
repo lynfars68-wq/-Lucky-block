08f5d7dd6420510c8f75f6cacc13bce682ba0c54
YAZED XODB <lynfars68@gmail.com>
2026-03-27 02:30:49 +0300

    خطأ بسكربت صلحتة

--- lucky block script.lua
+++ lucky block script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/lynfars68-wq/44559c50b893b3b4f1fb81dea62d01c2/raw/aa058997cd37edae1dbf10bbf5c3eb6ba7d063a2/gistfile1.txt", true))("🍀")
+loadstring(game:HttpGet("https://gist.githubusercontent.com/lynfars68-wq/bee5b1724c729509280062c837b91fac/raw/8e9c17fc377ad69744e6aae75d61604916da136c/gistfile1.txt", true))("🍀")
